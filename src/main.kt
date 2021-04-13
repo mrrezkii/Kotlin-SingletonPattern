a06f7111dc4502fedc4a882a4004aecf3a8e0667
nando <muhammad.rezki.ananda@gmail.com>
2021-04-13 09:20:40 +0700

    Using object

--- src/main.kt
+++ src/main.kt
@@ -1,10 +1,6 @@
-class SingletonApp private constructor() {
-    companion object {
-        private var instance: SingletonApp? = null
-        operator fun invoke() = synchronized(this) {
-            if (instance == null) instance = SingletonApp()
-            instance
-        }
+object SingletonApp {
+    init {
+        println("Singleton Pattern Initialized")
     }
 }
 
